af69141e1dcafb091f4d36bad5ee814ae514cd86
BrendanFinnegan <bf289907@gmail.com>
2021-05-03 17:05:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newElly = document.createElement('h1');
+document.body.appendChild(newElly);
+newElly.id = 'victory'
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = 'Brendan is the champion'
